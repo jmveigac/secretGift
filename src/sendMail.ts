9a3052a5f2b82a9e00d78ab6d559a32504dade06
Jose Manuel Veiga Castillo <44842389+jmveigac@users.noreply.github.com>
2026-06-29 11:26:30 +0200

    Fail fast on mail delivery errors

--- src/sendMail.ts
+++ src/sendMail.ts
@@ -3,27 +3,21 @@ import envUtils from './utils/envUtils';
 import Mail from './models/mail';
 
 export const sendMail = async (mail: Mail) => {
-    try {
-        const transporter = nodemailer.createTransport({
-            host: envUtils.HOST,
-            port: envUtils.PORT,
-            secure: true,//true for 465, false for other ports
-            auth: {
-                user: envUtils.MAILUSER,
-                pass: envUtils.MAILPWD
-            }
-        } as TransportOptions);
+    const transporter = nodemailer.createTransport({
+        host: envUtils.HOST,
+        port: Number(envUtils.PORT),
+        secure: Number(envUtils.PORT) === 465,
+        auth: {
+            user: envUtils.MAILUSER,
+            pass: envUtils.MAILPWD
+        }
+    } as TransportOptions);
 
-        const info = await transporter.sendMail({
-            from: envUtils.FROM,
-            to: mail.destinyMail,
-            subject: mail.subject,
-            text: mail.messageText,
-            html: mail.messageHtml
-        });
-
-        return info;
-    } catch (error) {
-        console.log(error);
-    }
+    return transporter.sendMail({
+        from: envUtils.FROM,
+        to: mail.destinyMail,
+        subject: mail.subject,
+        text: mail.messageText,
+        html: mail.messageHtml
+    });
 };
